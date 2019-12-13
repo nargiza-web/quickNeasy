c75c7979b97a2450e80f59e65d70facf96b327c5
Nargiza O <nargiza2k@gmail.com>
2019-12-13 15:10:30 -0600

    hi

--- scripts/auth.js
+++ scripts/auth.js
@@ -61,7 +61,7 @@ const logout = document.querySelector('#logout');
 logout.addEventListener('click', (e)=>{
   e.preventDefault();
   auth.signOut().then(() => {
-    window.location = "fakeindex.html"
+    window.location = "index.html"
   })
 })
 
